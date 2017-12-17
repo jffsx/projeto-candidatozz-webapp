44a52948ce39be9871f1e3338f110496b9e1ba4d
Jefferson <jefferson.andrade.agostinho@gmail.com>
2017-12-17 14:16:57 -0200

    Added toast messages

--- src/main.js
+++ src/main.js
@@ -5,14 +5,17 @@ import App from './App'
 import router from './router'
 import BootstrapVue from 'bootstrap-vue'
 import VueResource from 'vue-resource'
+import VueToastr from '@deveodk/vue-toastr'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import '@deveodk/vue-toastr/dist/@deveodk/vue-toastr.css'
 
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
 Vue.use(VueResource)
+Vue.use(VueToastr, { defaultPosition: 'toast-top-right', defaultType: 'info', defaultTimeout: 6000 })
 
 Vue.http.interceptors.push((request, next) => {
   request.headers.set('Accept', 'application/json')
