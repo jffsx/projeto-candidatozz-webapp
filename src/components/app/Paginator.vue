016ec5c7ddbc07e8a8967ff0253c8b38c2011ff8
Jefferson <jefferson.andrade.agostinho@gmail.com>
2017-12-17 23:37:50 -0200

    Fix paginator

--- src/components/app/Paginator.vue
+++ src/components/app/Paginator.vue
@@ -1,13 +1,14 @@
 <template>
 
-  <div class="paginatior" v-if="">
+  <div class="paginatior" v-if="this.pagination.next_page_url || this.pagination.prev_page_url">
     <button class="btn btn-default" @click="fetchData(pagination.prev_page_url)"
-            :disabled="!pagination.prev_page_url">
-        Anterior
+      :disabled="!pagination.prev_page_url">
+      Anterior
     </button>
-    <span> Página {{pagination.current_page}} de {{pagination.total_page}}</span>
+    <span> Página {{pagination.current_page}} de {{pagination.last_page}}</span>
     <button class="btn btn-default" @click="fetchData(pagination.next_page_url)"
-            :disabled="!pagination.next_page_url">Pŕoximo
+      :disabled="!pagination.next_page_url">
+      Pŕoximo
     </button>
   </div>
 
@@ -25,6 +26,7 @@ export default {
     return {
       pagination: {
         current_page: null,
+        last_page: null,
         total_page: null,
         first_page_url: null,
         last_page_url: null,
@@ -40,6 +42,7 @@ export default {
       this.$http.get(pageUrl)
         .then(function (response) {
           this.pagination.current_page = response.body.current_page
+          this.pagination.last_page = response.body.last_page
           this.pagination.total_page = response.body.total_page
           this.pagination.first_page_url = response.body.first_page_url
           this.pagination.last_page_url = response.body.last_page_url
@@ -47,8 +50,6 @@ export default {
           this.pagination.prev_page_url = response.body.prev_page_url
 
           this.$emit('update', response.body.data)
-        }).catch(function (response) {
-          console.log('response list error', response)
         })
     }
   },
